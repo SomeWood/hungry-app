fff8c6e9705d1aed719d33909adea094d40bf5b4
Matthew <lmbalisacan@gmail.com>
2026-03-02 19:49:13 +0800

    added urls and base.html

--- hungry_app/hungry_app/urls.py
+++ hungry_app/hungry_app/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+    path('dashboard/', include('dashboard.urls')),
+    path('food/', include('food.urls')),
+    path('exercise/', include('exercise.urls')),
+    path('weight/', include('weight.urls')),
     path('admin/', admin.site.urls),
 ]
