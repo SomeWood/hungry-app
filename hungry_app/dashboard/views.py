6e6fee82c270bc93422f097221f6fc63b6f57dbb
Samuel Wood <woodsamueldrury@gmail.com>
2026-04-12 17:22:06 +0800

    Add net calorie computation in the dashboard.

--- hungry_app/dashboard/views.py
+++ hungry_app/dashboard/views.py
@@ -1,7 +1,30 @@
+import datetime
+
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+
+from exercise.models import ExerciseEntry
+from food.models import FoodEntry
 
 # Create your views here.
 @login_required
 def dashboard_view(request):
-    return render(request, 'dashboard/dashboard.html')
+    selected_date = request.GET.get('date')
+
+    if selected_date:
+        food_entries = FoodEntry.objects.filter(user=request.user, date=selected_date)
+        exercise_entries = ExerciseEntry.objects.filter(user=request.user, date=selected_date)
+    else:
+        selected_date = datetime.date.today()
+        food_entries = FoodEntry.objects.filter(user=request.user, date=selected_date)
+        exercise_entries = ExerciseEntry.objects.filter(user=request.user, date=selected_date)
+
+    total_calories = 0
+    for entry in food_entries:
+        total_calories += entry.calories
+    for entry in exercise_entries:
+        total_calories -= entry.calories
+
+    context = {'total_calories' : total_calories}
+
+    return render(request, 'dashboard/dashboard.html', context)
