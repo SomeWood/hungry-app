f5fdae1aca9f9267d5d8eb3c7b32d14f34bae691
Samuel Wood <woodsamueldrury@gmail.com>
2026-04-12 16:42:26 +0800

    Added urls.

--- hungry_app/exercise/urls.py
+++ hungry_app/exercise/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('', views.exercise_log, name='exercise_log'),
+    path('add/', views.add_exercise_entry, name='add_exercise_entry'),
+    path('delete/<int:entry_id>/', views.delete_exercise_entry, name='delete_exercise_entry'),
 ]
